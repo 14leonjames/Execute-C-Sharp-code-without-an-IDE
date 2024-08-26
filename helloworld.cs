e0338dc7075bdcd572fd65b65cdadf32f5d2a567
pit_viper <54982092+14leonjames@users.noreply.github.com>
2024-08-26 22:35:59 +0530

    Add files via upload

---
+++ helloworld.cs
@@ -0,0 +1,18 @@
+// C# program to print Hello World! 
+using System; 
+
+namespace HelloWorldApp { 
+
+class Geeks { 
+
+	static void Main(string[] args) 
+	{ 
+
+		// printing Hello World! 
+		Console.WriteLine("Hello World!"); 
+
+		// To prevents the screen from running and closing quickly 
+		Console.ReadKey(); 
+	} 
+} 
+} 
